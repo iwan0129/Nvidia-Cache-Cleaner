4b79ebbeb6bf575618a2090b9ec6dfec39ce07f3
Iwan0129 <uzunow0129@gmail.com>
2020-12-03 23:32:18 +0200

    Improved Cache Cleaner Added array containing the cache locations Added D3DSCache location Added exception catch statement Added output to tell the user the progress of the clearing

--- Nvidia Cache Cleaner/Nvidia_Cache_Cleaner.py
+++ Nvidia Cache Cleaner/Nvidia_Cache_Cleaner.py
@@ -1,15 +1,26 @@
-import os
-import shutil
-from os import path
+import os;
+import shutil;
+from os import path;
 
-nvcache_path = path.expandvars(r"%LOCALAPPDATA%\NVIDIA Corporation\NV_Cache")
+cache_paths =[path.expandvars(r'%LOCALAPPDATA%\NVIDIA Corporation\NV_Cache'), 
+              path.expandvars(r'%LOCALAPPDATA%\D3DSCache')];
 
-def clear_cache(dir):
+def clear_path(dir):
     for root, dirs, files in os.walk(dir):
-        for file in files:
-            os.unlink(path.join(root, file))
-        for folder in dirs:
-            shutil.rmtree(path.join(root, folder))
+        try:
+            for file in files:
+                os.unlink(path.join(root, file));
+            for folder in dirs:
+                shutil.rmtree(path.join(root, folder));
+        except Exception as ex:
+            print('{0}\n'.format(ex.args));
 
-if (path.exists(nvcache_path)):
-    clear_cache(nvcache_path)
+for cache_path in cache_paths:
+    if (path.exists(cache_path)):
+        print('Clearing: {0}\n'.format(cache_path));
+        clear_path(cache_path);    
+        
+print('Done Clearing, You Can Close The Program Now.\n');
+
+while True:
+    pass;
