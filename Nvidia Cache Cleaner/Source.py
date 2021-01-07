338fea303a7b92e8c4470b6db5f7fa6e3de54a23
Iwan0129 <uzunow0129@gmail.com>
2021-01-07 21:33:29 +0200

    Improved Exception Handling & Added New Location For Clearing

--- Nvidia Cache Cleaner/Source.py
+++ Nvidia Cache Cleaner/Source.py
@@ -1,26 +1,37 @@
-import os;
-import shutil;
-from os import path;
+import os
+import shutil
+from os import path
 
-cache_paths =[path.expandvars(r'%LOCALAPPDATA%\NVIDIA Corporation\NV_Cache'), 
-              path.expandvars(r'%LOCALAPPDATA%\D3DSCache')];
+cache_paths = [path.expandvars(r'%LOCALAPPDATA%\NVIDIA Corporation\NV_Cache'), 
+              path.expandvars(r'%LOCALAPPDATA%\D3DSCache'),
+              path.expandvars(r'%PROGRAMDATA%\NVIDIA Corporation\NV_Cache')]
 
 def clear_path(dir):
     for root, dirs, files in os.walk(dir):
-        try:
-            for file in files:
+        for file in files:
+            try:
                 os.unlink(path.join(root, file));
-            for folder in dirs:
-                shutil.rmtree(path.join(root, folder));
-        except Exception as ex:
-            print('{0}\n'.format(ex.args));
+            except Exception as ex:
+                print('{0}\n'.format(ex.args))
+                continue;
+            pass;
 
+        for folder in dirs:
+            try:
+                shutil.rmtree(path.join(root, folder))
+            except Exception as ex:
+                print('{0}\n'.format(ex.args))
+                continue;
+            pass;
+        pass; 
+pass;
+                
 for cache_path in cache_paths:
     if (path.exists(cache_path)):
-        print('Clearing: {0}\n'.format(cache_path));
-        clear_path(cache_path);    
+        print('Clearing: {0}\n'.format(cache_path))
+        clear_path(cache_path)    
         
-print('Done Clearing, You Can Close The Program Now.\n');
+print('Done Clearing, You Can Close The Program Now.\n')
 
 while True:
     pass;
